ff4b7f36068d6237ad7df4fdefa21e6d224f33ce
Luyu Wang <wanglouis49@gmail.com>
2016-05-03 23:24:14 -0400

    EX10 finished

--- EX10.py
+++ EX10.py
@@ -15,9 +15,9 @@ class EX10(object):
 		self.S0 = 100.; self.mu = 0.08; self.sigma = 0.3
 		self.rfr = 0.05; self.T = 0.1; self.tau = 0.04
 		self.K = 100.; self.H = 95.
-		self.c = 306.8763; self.perc = 0.99
+		self.c = 246.42939112403042; self.perc = 0.99
 
-		self.I_pred = 2.**15
+		self.I_pred = 2**15
 
 		# --- portfolio price @ t = 0 ---
 		opt_val = lambda S: - 5.*bp.blsprice(S,self.K,self.rfr,self.T,self.sigma,'put') \
@@ -73,7 +73,6 @@ class EX10(object):
 		S1[:,:] = S1[:,:] * np.exp((self.mu - 0.5*self.sigma*self.sigma)*self.tau + self.sigma *\
 								np.sqrt(self.tau) * ran1[:,:])
 
-		t0 = time.time()
 		ValueTau = np.zeros((N_o,1))
 		for dim in range(self.D):
 			ran2 = npr.standard_normal((N_o,N_i))
@@ -106,10 +105,69 @@ class EX10(object):
 		return (eel, t_ns)
 
 	def regr_data_prep(self,kk,N_i=1):
-		pass
+		''' Regression data preparation via nested simulations
+		'''
+		# --- Computation budget allocation ---
+		N_o = int(kk/N_i)
+
+		# --- portfolio loss distribution @ t = \tau via analytical formulas---
+		t0 = time.time()
+		ran1 = npr.standard_normal((N_o,self.D))
+		S1 = np.zeros((N_o,self.D))
+		S1[:,:] = self.S0
+		S1[:,:] = S1[:,:] * np.exp((self.mu - 0.5*self.sigma*self.sigma)*self.tau + self.sigma *\
+								np.sqrt(self.tau) * ran1[:,:])
+
+		ValueTau = np.zeros((N_o,1))
+		for dim in range(self.D):
+			ran2 = npr.standard_normal((N_o,N_i))
+			S2 = np.zeros((N_o,N_i))
+			S2[:,:] = np.dot(S1[:,dim,np.newaxis],np.ones((1,N_i))) * np.exp((self.rfr - 0.5*self.sigma**2)*(self.T-self.tau) + self.sigma \
+				* np.sqrt(self.T-self.tau) * ran2[:,:])
+
+			prob = (1.-np.exp(-2.*(np.log(np.dot(S1[:,dim,np.newaxis],np.ones((1,N_i)))/self.H)\
+			                   *np.log(S2[:,:]/self.H)/(self.sigma**2)/(self.T-self.tau))))\
+		                       *(np.dot(S1[:,dim,np.newaxis],np.ones((1,N_i))) >= self.H*np.ones((N_o,N_i))).astype(float)\
+		                       *(S2[:,:] >= self.H*np.ones((N_o,N_i))).astype(float)
+
+			C2do = np.dot((np.maximum(S2[:,:]-self.K,0)*prob), np.ones((N_i,1))) / \
+				float(N_i) * np.exp(-self.rfr*(self.T-self.tau))
+
+			P2 = np.dot((np.maximum(self.K-S2[:,:],0)),np.ones((N_i,1))) / \
+				float(N_i) * np.exp(-self.rfr*(self.T-self.tau))
+
+			ValueTau[:] += -10. * C2do - 5. * P2
+		t_ns = time.time() - t0
+
+		ran3 = norm(loc=0,scale=1).ppf(lhs(self.D,samples=self.I_pred))
+		S_pred = np.zeros((self.I_pred,self.D))
+		S_pred[:,:] = self.S0
+		S_pred[:,:] = S_pred[:,:] * np.exp((self.mu - 0.5*self.sigma*self.sigma)*self.tau + self.sigma *\
+								np.sqrt(self.tau) * ran3[:,:])
+
+		self.X = S1
+		self.X_pred = S_pred
+		self.y = ValueTau
+
 
 	def poly_regr(self,deg=2):
-		pass
+		''' Polynomial Regression
+		'''
+		# Training
+		t0 = time.time()
+		phi = cm.naivePolyFeature(self.X,deg=deg,norm=True)
+		U, s, V = np.linalg.svd(phi,full_matrices=False)
+		r = np.dot(V.T,np.dot(U.T,self.y)/s[:,np.newaxis])
+		t_tr = time.time() - t0
+
+		# Predicting
+		t0 = time.time()
+		phi_pred = cm.naivePolyFeature(self.X_pred,deg=deg,norm=True)
+		y_lr = np.dot(phi_pred,r)
+		t_pr = time.time() - t0
+
+		eel = np.mean(np.maximum(self.Value0-self.c-np.sum(y_lr,axis=1),0))
+		return (eel, t_tr, t_pr)
 
 	def spec_regr(self,deg=5):
 		pass
@@ -118,15 +176,112 @@ def re_ns(kk):
 	pass
 
 def re_poly2(kk,N_i):
-	pass
+	from EX10 import EX10
+	import time
+	if kk/N_i < 1.:
+		eel = (np.nan, 0., 0., 0.)
+	else:
+		port = EX10()
+		t0 = time.time()
+		port.regr_data_prep(kk,N_i)
+		t_ns = time.time() - t0
+		eel = port.poly_regr(deg=2)
+		eel += (t_ns,)
+	return eel
 
-def re_spec5(kk,N_i):
-	pass
+def re_poly5(kk,N_i):
+	from EX10 import EX10
+	import time
+	if kk/N_i < 1.:
+		eel = (np.nan, 0., 0., 0.)
+	else:
+		port = EX10()
+		t0 = time.time()
+		port.regr_data_prep(kk,N_i)
+		t_ns = time.time() - t0
+		eel = port.poly_regr(deg=5)
+		eel += (t_ns,)
+	return eel
+
+def re_poly8(kk,N_i):
+	from EX10 import EX10
+	import time
+	if kk/N_i < 1.:
+		eel = (np.nan, 0., 0., 0.)
+	else:
+		port = EX10()
+		t0 = time.time()
+		port.regr_data_prep(kk,N_i)
+		t_ns = time.time() - t0
+		eel = port.poly_regr(deg=8)
+		eel += (t_ns,)
+	return eel
 
 def conv(K,N_i=1,L=100,regr_method=re_poly2,filename='EX10'):
-	pass
+	import scipy.io
+	import ipyparallel
+	from multiprocessing import cpu_count
+	import os
+
+	print
+	print "##################################"
+	print "# Risk Estimation via Regression #"
+	print "##################################"
+	print
+	print "regression method: %s" % str(regr_method)
+	print "Output file: %s.mat" % filename
+	print "N_i = %d; L = %d" % (N_i, L)
+	print
+	print "Setting up ipyparallel"
+	print "CPU count: %d" % cpu_count()
+	print
+	rc = ipyparallel.Client()
+	print("Check point 1")
+	rc.block = True
+	print("Check point 2")
+	view = rc.load_balanced_view()
+	print("Check point 3")
+	dview = rc[:]
+	print("Check point 4")
+	dview.map(os.chdir, ['/home/l366wang/Code/risk_regr/']*cpu_count())
+	print("Check point 5")
+	print
+	print "Checks done. Commensing computations..."
+	print
+
+
+	EEL_true = 0.45553892616275565
+
+	mse = np.zeros(len(K))
+	bias2 = np.zeros(len(K))
+	var = np.zeros(len(K))
+	t_tr = np.zeros(len(K))
+	t_pr = np.zeros(len(K))
+	t_ns = np.zeros(len(K))
+
+	for k_idx, kk in enumerate(K):
+		print "K = %d" % kk
+		t0 = time.time()
+		eel_data = view.map(regr_method,[kk]*L,[N_i]*L)
+		eel = [eel_data[ii][0] for ii in range(L)]
+
+		mse[k_idx] = np.mean((np.array(eel)-EEL_true)**2)
+		bias2[k_idx] = (np.mean(eel)-EEL_true)**2
+		var[k_idx] = np.mean((np.array(eel)-np.mean(eel))**2)
+
+		t_tr[k_idx] = np.mean([eel_data[ii][1] for ii in range(L)])
+		t_pr[k_idx] = np.mean([eel_data[ii][2] for ii in range(L)])
+		t_ns[k_idx] = np.mean([eel_data[ii][3] for ii in range(L)])
+
+		print "%.2fs elapsed" % (time.time()-t0)
+		scipy.io.savemat('./Data/EX10/'+filename+'.mat',mdict={'K':K,'N_i':N_i,'L':L,\
+			'mse':mse,'bias2':bias2,'var':var,'t_tr':t_tr, 't_pr':t_pr,\
+			't_ns':t_ns})
+		print
 
 if __name__ == "__main__":
 	import EX10
-	K = [ii**5 for ii in range(4,23)]
-	EX10.conv(K,N_i=1,L=100,regr_method=re_poly2,filename='EX10')
+	K = [ii**5 for ii in range(2,23)]
+	EX10.conv(K,N_i=1,L=100,regr_method=re_poly2,filename='re_poly2_1')
+	EX10.conv(K,N_i=1,L=100,regr_method=re_poly5,filename='re_poly5_1')
+	EX10.conv(K,N_i=1,L=100,regr_method=re_poly8,filename='re_poly8_1')
