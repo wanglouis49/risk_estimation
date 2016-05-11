f7334cdebff313a02fd11c35049250d3e82a2cdf
Luyu Wang <wanglouis49@gmail.com>
2016-05-11 19:19:49 -0400

    diver.py updated

--- driver.py
+++ driver.py
@@ -4,11 +4,11 @@
 #EX1.conv(K, N_i=1, L=1000, regr_method=EX10.re_poly5, filename='re_poly5_1')
 #EX1.conv(K, N_i=1, L=1000, regr_method=EX10.re_poly8, filename='re_poly8_1')
 
-import EX10
-K = [ii**5 for ii in range(2,23)]
-EX10.conv(K,N_i=1,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_1')
-EX10.conv(K,N_i=10,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_10')
-EX10.conv(K,N_i=100,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_100')
+#import EX10
+#K = [ii**5 for ii in range(2,23)]
+#EX10.conv(K,N_i=1,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_1')
+#EX10.conv(K,N_i=10,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_10')
+#EX10.conv(K,N_i=100,L=100,regr_method=EX10.re_svrCV,filename='re_svrCV_100')
 
 import EX100V2c as EX100
 K = [ii**5 for ii in range(2,20)]
@@ -21,3 +21,13 @@ EX100.conv(K,N_i=10,L=100,regr_method=EX100.re_poly8,filename='re_poly8_10')
 EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_poly2,filename='re_poly2_100')
 EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_poly5,filename='re_poly5_100')
 EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_poly8,filename='re_poly8_100')
+
+EX100.conv(K,N_i=1,L=100,regr_method=EX100.re_ridge2,filename='re_ridge2_1')
+EX100.conv(K,N_i=1,L=100,regr_method=EX100.re_ridge5,filename='re_ridge5_1')
+EX100.conv(K,N_i=1,L=100,regr_method=EX100.re_ridge8,filename='re_ridge8_1')
+EX100.conv(K,N_i=10,L=100,regr_method=EX100.re_ridge2,filename='re_ridge2_10')
+EX100.conv(K,N_i=10,L=100,regr_method=EX100.re_ridge5,filename='re_ridge5_10')
+EX100.conv(K,N_i=10,L=100,regr_method=EX100.re_ridge8,filename='re_ridge8_10')
+EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_ridge2,filename='re_ridge2_100')
+EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_ridge5,filename='re_ridge5_100')
+EX100.conv(K,N_i=100,L=100,regr_method=EX100.re_ridge8,filename='re_ridge8_100')
